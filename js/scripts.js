fd5bf125a1f4b413cd5e26667bc2c1a2b8b32795
tobiasjberg <47767464+tobiasjberg@users.noreply.github.com>
2020-04-30 20:41:37 -0500

    color scheme changer

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,25 @@ document.getElementById("nav-menu").addEventListener("click", function(){
         navlist.style.display = "none";
     }
 });
+var schemelist = ["colorscheme1","colorscheme2","colorscheme3"];
+document.getElementById("ccsbtn").addEventListener("click", function(){
+	var cl = document.getElementById("fullwrap").classList;
+	var key, value, result;
+	for (key in cl) {
+	    if (cl.hasOwnProperty(key) && !isNaN(parseInt(key, 10))) {
+	        value = cl[key];
+	        if (value.substring(0, 11) === "colorscheme") {
+	            result = value;
+	            break;
+	        }
+	    }
+	} //console.log("result = "+result);
+	var sn = parseInt(result.substring(result.length-1)); //console.log("sn = "+sn);
+	if (sn >= schemelist.length) {
+		sn = 1;
+	} else {
+		sn = sn+1;
+	} //console.log("sn = "+sn);
+	document.getElementById("fullwrap").classList.remove(result);
+	document.getElementById("fullwrap").classList.add("colorscheme"+sn.toString());
+});
